9efd52b0c3a143d1a454c07557997fa50b0723db
thiago_sena <thiago.sena@itsstecnologia.com.br>
2019-08-18 23:16:59 -0300

    first commit

---
+++ src/main/java/br/com/cashierdocmanager/cashierdocmanager/CashierDocManagerApplication.java
@@ -0,0 +1,13 @@
+package br.com.cashierdocmanager.cashierdocmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashierDocManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashierDocManagerApplication.class, args);
+	}
+
+}
