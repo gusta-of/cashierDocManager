8d18592116f429319d5d511654b4ace5394bbddf
thiago_sena <thiago.sena@itsstecnologia.com.br>
2019-08-21 23:44:53 -0300

    Reestruturação do projeto

--- src/main/java/mainProject/CashierDocManagerApplication.java
+++ src/main/java/mainProject/CashierDocManagerApplication.java
@@ -1,4 +1,4 @@
-package br.com.cashierdocmanager.cashierdocmanager.apresentacao.main;
+package mainProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
