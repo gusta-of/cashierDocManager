5a0816b9c412639f842624ce0e08c47e4090563c
Gustavo Fernandes <gustavoofernandes9712@gmail.com>
2019-08-29 23:28:01 -0300

    gustavo-fernandes: configura entidade para banco

--- src/main/java/modelo/Pessoa.java
+++ src/main/java/modelo/Pessoa.java
@@ -1,15 +1,31 @@
 package modelo;
 
+import javax.naming.Name;
+import javax.persistence.*;
+import java.util.Date;
 import java.util.Objects;
 
+@Entity
 public class Pessoa {
 
     public Pessoa() {
     }
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long ID;
+
+    @Column(name="nome", nullable = false)
     private String Nome;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name="dataNascimento", nullable = false)
+    private Date DataNascimento;
+
+    @Column(name="cpf", nullable = false)
     private String CPF;
+
+    @Column(name="email", nullable = false)
     private String Email;
 
     public Long getID() {
@@ -42,6 +58,14 @@ public class Pessoa {
 
     public String getEmail() {
         return Email;
+    }
+
+    public Date getDataNascimento() {
+        return DataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        DataNascimento = dataNascimento;
     }
 
     @Override
