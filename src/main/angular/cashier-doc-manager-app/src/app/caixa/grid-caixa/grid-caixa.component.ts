d5b56eb3808c5c236951ddd6afe99e06dca5776e
thiagoSena <thiago.sena@itsstecnologia.com.br>
2019-09-14 16:54:44 -0300

    Adição Grid

--- src/main/angular/cashier-doc-manager-app/src/app/caixa/grid-caixa/grid-caixa.component.ts
+++ src/main/angular/cashier-doc-manager-app/src/app/caixa/grid-caixa/grid-caixa.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProdutoModel } from 'src/app/model/produto.model.component';
 
 @Component({
   selector: 'grid-caixa-CDM',
@@ -10,8 +11,28 @@ export class GridCaixaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    this.recebeValores();
   }
 
-  lista : any []= [];
+  lista : ProdutoModel []= [];
+
+
+   recebeValores() {
+    
+    let produtoModel = new ProdutoModel;
+
+    for (let index = 0; index < 200 ; index++) {
+
+      produtoModel.produtoNome = 'DIPIRONA' + '1 + 1';
+
+      produtoModel.quantidade = 10 + 1;
+  
+      produtoModel.total= 100 + 1;
+
+      this.lista.push(produtoModel);
+  }  
+
+
+  }
 
 }
